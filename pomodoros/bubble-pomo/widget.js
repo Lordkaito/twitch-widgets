b6a21415a8f70c4cbde334004c87e7592b831d7d
Lordkaito <cespedesisai88@gmail.com>
2024-05-31 14:39:44 +0200

    Update pomo counter

--- pomodoros/bubble-pomo/widget.js
+++ pomodoros/bubble-pomo/widget.js
@@ -2,6 +2,9 @@ const button = document.querySelector(".click")
 const hoursContainer = document.querySelector(".hours-container")
 const minutesContainer = document.querySelector(".minutes-container")
 const secondsContainer = document.querySelector(".seconds-container")
+const hoursSeparator = document.querySelector(".hours-separator")
+const minutesSeparator = document.querySelector(".minutes-separator")
+const pomosCounter = document.querySelector(".pomos-left")
 
 button.addEventListener("click", () => {
   startPomo()
@@ -14,10 +17,29 @@ secondsContainer.textContent = secondsLeft.toString().padStart(2, "0")
 minutesContainer.textContent = minutesLeft.toString().padStart(2, "0")
 hoursContainer.textContent = hoursLeft.toString().padStart(2, "0")
 const maxPomos = 4 // this will be set to the fieldData value later
+let currentPomo = 0
+
+const fieldDataHours = 1
+const fieldDataMinutes = 20
+const fieldDataSeconds = 0
+let isRunning = false
 
 // the padStart method adds a zero to the left of the number if it is less than 10
 let hoursTimer, minutesTimer, secondsTimer
 const startPomo = () => {
+  if(isRunning) return
+  isRunning = true
+  console.log(hoursLeft, minutesLeft, secondsLeft)
+  if(hoursLeft === 0 && minutesLeft === 0 && secondsLeft === 60) {
+    hoursLeft = fieldDataHours
+    minutesLeft = fieldDataMinutes
+    secondsLeft = fieldDataSeconds
+  }
+  pomosCounter.textContent = `${currentPomo}|${maxPomos}`
+  if(hoursLeft <= 0) {
+    hoursContainer.style.display = "none"
+    hoursSeparator.style.display = "none"
+  }
   secondsTimer = setInterval(updateSeconds, 1000)
 }
 
@@ -38,7 +60,6 @@ const updateMinutes = () => {
 }
 
 const updateSeconds = () => {
-  console.log(secondsLeft)
   secondsLeft--
 
   if (secondsLeft === 59) {
@@ -58,7 +79,12 @@ const updateSeconds = () => {
     secondsContainer.textContent = secondsLeft.toString().padStart(2, "0")
   }
   const isOver = hoursLeft === 0 && minutesLeft === 0 && secondsLeft === 60
+  if(isOver && maxPomos > currentPomo) {
+    currentPomo++
+    pomosCounter.textContent = `${currentPomo}|${maxPomos}`
+  }
   if (isOver) {
+    isRunning = false
     clearInterval(secondsTimer)
   }
 }
--- tasklists/brown-tasks/widget.js
+++ tasklists/brown-tasks/widget.js
@@ -47,7 +47,13 @@ const addTaskToList = task => {
   const taskItem = `
   <div class="flex-wrap" id=${task.id}>
     <div class="task">
-      <input type="checkbox">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+    class="icon icon-tabler icons-tabler-outline icon-tabler-checkbox">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M9 11l3 3l8 -8" />
+    <path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9" />
+  </svg>
       <p class="task-title">${task.task}</p>
     </div>
   </div>`
